9536f87bec3e77c62c8b18c685ba103ea294cf80
Artur Shomarov <artur.shomarov@metropolia.fi>
2018-09-27 10:37:16 +0300

    new classes

--- Palkanseuraaja/src/main/java/models/WorkEvent.java
+++ Palkanseuraaja/src/main/java/models/WorkEvent.java
@@ -10,6 +10,8 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
 import javax.persistence.Table;
 
 @Entity
@@ -26,7 +28,9 @@ public class WorkEvent {
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
     
-    
+    @OneToMany(cascade = CascadeType.ALL, targetEntity = WorkProfile.class)
+    @JoinColumn(name = "tyoprofiili_id")
+    private WorkProfile tyoprofiili;
     
     @ManyToOne(cascade = CascadeType.ALL, targetEntity = Palkka.class)
     @JoinColumn(name = "palkka_id", nullable = false)
@@ -35,5 +39,58 @@ public class WorkEvent {
     private Timestamp timestampStart; // not sure about timestamp, must check
     
     private Timestamp timestampEnd;   // not sure about timestamp, must check
+
+    public WorkEvent() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public WorkProfile getTyoprofiili() {
+        return tyoprofiili;
+    }
+
+    public void setTyoprofiili(WorkProfile tyoprofiili) {
+        this.tyoprofiili = tyoprofiili;
+    }
+
+    public Palkka getPalkka() {
+        return palkka;
+    }
+
+    public void setPalkka(Palkka palkka) {
+        this.palkka = palkka;
+    }
+
+    public Timestamp getTimestampStart() {
+        return timestampStart;
+    }
+
+    public void setTimestampStart(Timestamp timestampStart) {
+        this.timestampStart = timestampStart;
+    }
+
+    public Timestamp getTimestampEnd() {
+        return timestampEnd;
+    }
+
+    public void setTimestampEnd(Timestamp timestampEnd) {
+        this.timestampEnd = timestampEnd;
+    }
+    
+    
 
 }
