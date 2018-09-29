0dadf197ebea2762a396db2c99bba76eab246394
Joni Halonen <jonpethal@gmail.com>
2018-09-29 12:32:28 +0300

    WorkProfileView ja Controller lisätty workProfileList

--- Palkanseuraaja/src/main/java/controllers/WorkProfileViewController.java
+++ Palkanseuraaja/src/main/java/controllers/WorkProfileViewController.java
@@ -2,21 +2,30 @@ package controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
 import javafx.scene.text.Font;
 
 /**
  * FXML Controller class
  *
- * @author artur
+ * @author artur, joni
  */
 public class WorkProfileViewController implements Initializable {
+	
+	ObservableList<String> workProfileList = FXCollections.observableArrayList("Profile1", "Profile2", "Profile3");
 
     @FXML
     private Font x12;
     @FXML
     private Font x121;
+    
+    @FXML
+    private ComboBox<String> tyoprofiiliDrop;
 
     /**
      * Initializes the controller class.
@@ -24,6 +33,9 @@ public class WorkProfileViewController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
+    	tyoprofiiliDrop.setValue("Profile1");
+    	tyoprofiiliDrop.setItems(workProfileList);
+    	
     }    
     
 }
