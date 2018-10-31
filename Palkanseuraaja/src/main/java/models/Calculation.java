bc67692f80cfd1b81fd461bca6a4e78b888c8e21
Joonas Eskonheimo <jeskonheimo@gmail.com>
2018-10-31 14:21:25 +0200

    Lisätty raakile palkkalisän laskennalle

--- Palkanseuraaja/src/main/java/models/Calculation.java
+++ Palkanseuraaja/src/main/java/models/Calculation.java
@@ -1,14 +1,16 @@
 package models;
 
+import java.util.Date;
+
 public class Calculation {
-	
-	
-	
-	
+
+
+
+
 	public Calculation() {
 		// TODO Auto-generated constructor stub
 	}
-	
+
 	public static double Calculate (EventModel event) {
 		long start;
 		long end;
@@ -35,10 +37,48 @@ public class Calculation {
 		pay = event.getWorkProfile().getPay()/60;
 		//lasketaan kokonaisansiot
 		pay = pay * minutes;
-			
-		return (double)Math.round(pay * 100d) / 100d;
-		
+
+		/*
+		 * Raakile palkkalisän laskemiselle
+		 *
+		 *
+		 * return (double)Math.round(pay * 100d) / 100d;
+		Date EAlku = new Date(2018 + 1900, 9, 20, 14,0,0 );
+		Date ELoppu = new Date(2018 + 1900, 9, 21, 2,0,0);
+		Date LAlku = new Date(2018 + 1900, 9, 20, 18,0,0);
+		Date LLoppu = new Date(2018 + 1900, 9, 21, 3,0,0);
+
+		long eAlku = EAlku.getTime();
+		long eLoppu = ELoppu.getTime();
+		long lAlku = LAlku.getTime();
+		long lLoppu = LLoppu.getTime();
+		long erotus;
+		//Jos eventti alkaa aikaisemmin kuin palkkalisä
+		if (eAlku < lAlku) {
+			//Jos eventti loppuu ennen palkkalisää
+			if(eLoppu < lLoppu) {
+				erotus = eLoppu - lAlku;
+			}
+			else {
+				erotus = lLoppu - lAlku;
+			}
+		}
+		// jos eventti alkaa myöhemmin kuin palkkalisä
+		else {
+			//jos eventti loppuu ennen palkkalisää
+			if (eLoppu < lLoppu) {
+				erotus = lLoppu - eAlku;
+			}
+			else {
+				erotus = eLoppu - eAlku;
+			}
+
+		}
+
+		erotus = (erotus/1000)/60;
+		System.out.println(erotus);*/
+
 	}
-	
+
 
 }
